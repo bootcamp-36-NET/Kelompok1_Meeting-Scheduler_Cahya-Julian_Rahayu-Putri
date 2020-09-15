e0c19cfdc3ad21be47eb51552b5c8d79dc59182e
rahayuputrip <cornelia.rahayuputri@gmail.com>
2020-09-15 22:21:52 +0700

    Employee VM dan Controller (blm cek)

---
+++ API/API/ViewModels/EmployeeVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class EmployeeVM
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string FullName { get; set; }
+        public string Gender { get; set; }
+        public string DepartmentName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset DeleteDate { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+        public bool isDelete { get; set; }
+    }
+}
